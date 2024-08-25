34c03a135f072beb001286e5ae274f494536b6d2
K-mBT <k.beattie87@gmail.com>
2024-08-25 16:13:07 +0930

    Changed the current weather section

--- src/script.js
+++ src/script.js
@@ -2,6 +2,34 @@ function changeWeather(response) {
   let temperatureElement = document.querySelector("#weather-app-temp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let dayElement = document.querySelector("#day");
+
+  let timestamp = response.data.time;
+  let date = new Date(timestamp * 1000);
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let formattedTime = `${hours}:${minutes}`;
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+
+  timeElement.innerHTML = formattedTime;
+  dayElement.innerHTML = currentDay;
+  windElement.innerHTML = response.data.wind.speed;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  conditionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
@@ -15,9 +43,10 @@ function searchCity(city) {
 function giveResults(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", giveResults);
+
+searchCity("Darwin");
